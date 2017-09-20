0d52519b3286d876814335098e5dd394c405d4d5
alex04071996 <ernest_matrix09@hotmail.com>
2017-09-19 20:40:19 -0500

    A

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-# POO_Burciaga
+<?php 
+	require_once "controllers/controller.php";
+	require_once "models/model.php";
+	require_once "models/crud.php";
+	
+
+
+	$objetomvc = new controller();
+	$objetomvc -> plantilla();
+
+ ?>
