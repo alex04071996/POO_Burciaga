602915b11576a77d2b59ed9c807e15f9f33110c5
ROBERTPG <ROBERTPG@users.noreply.github.com>
2017-09-21 22:48:51 -0500

    Add files via upload

--- views/modules/contacto.php
+++ views/modules/contacto.php
@@ -1,1 +1,21 @@
-pagina contacto
+    <section>      
+        <div class="span9 md_contact_data  bs_box_bluid bs_box_block">
+          <i></i>
+          <h4> Horario de Atención</h4>
+          <p>
+            <span  class=" bs_box_bluid bs_box_block">Oficinas Administrativas</span>
+            Lunes a Viernes 8:00 am - 6:00 pm
+            Sábados 8:00 am - 4:00 pm
+          </p>
+          <h4>Teléfonos</h4>
+          <p>
+            (87) 214-1065 / (87) 122-4949
+
+          </p>
+          <h4>Dónde Estamos</h4>
+          <p>
+            Universidad Politecnica de Gomez Palacio, edificio B ITI
+          </p>
+        </div>
+
+    </section>
--- views/modules/nosotros.php
+++ views/modules/nosotros.php
@@ -1,1 +1,16 @@
-pagina nosotros
+     <article>
+        <div class="container">
+          <h3>QUIENES SOMOS</h3>
+          <span></span>
+        </div>
+        <div class="container">
+          <div class="container">
+            <P>La empresa eclipsim ITI se inició en el segundo alo de formacion de la carrera de ingenieria en tecnologias de la informacion desarrollando sistemas informaticos en el año 2017, actualmente  contamos con proyectos desarrollados en paginas web .</P>
+            <P>Gracias a la enseñanza del maestro burciaga y tambien el esfuerzo de sus colaboradores que son capacitados continuamente, el Grupo eclipsim brinda servicios profesionales de creacion de paginas web. </P>
+            <h4>MISIÓN</h4>
+            <p>Desarrollar y ofrecer productos de software integrales y de calidad para empresas en crecimiento y consolidación.</p>
+            <h4>VISIÓN</h4>
+            <p>Convertirnos en el líder regional y nacional en provisión de soluciones tecnológicas de última generación.</p>
+          </div>
+        </div>
+      </article>
