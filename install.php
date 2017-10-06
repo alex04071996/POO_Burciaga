9ac5e2369321b3b7719c1caffb5dd9182b7ec467
ROBERTPG <ROBERTPG@users.noreply.github.com>
2017-10-05 21:59:19 -0500

    Add files via upload
    base de datos - tabla productos

---
+++ install.php
@@ -0,0 +1,26 @@
+<?php
+	require_once 'includes/conexion.php';
+	
+	$sql = "CREATE TABLE IF NOT EXISTS productos(
+			id	int(255) auto_increment not null,
+			nombre		varchar(50),
+			precio      varchar(50),
+			CONSTRAINT pk_usuario PRIMARY KEY(id));";
+	
+	$create_productos_table = mysqli_query($con, $sql);
+	
+	$sql="INSERT INTO productos VALUES (NULL, 'manzana', '10' ";
+	mysqli_query($con, $sql);
+	$sql="INSERT INTO productos VALUES (NULL, 'naranja', '15' ";
+	mysqli_query($con, $sql);
+    $sql="INSERT INTO productos VALUES (NULL, 'galletas', '30' ";
+	mysqli_query($con, $sql);
+    $sql="INSERT INTO productos VALUES (NULL, 'refrsco', '20' ";
+	mysqli_query($con, $sql);
+	
+	if($create_productos_table){
+		echo "La Tabla de productos se ha creado correctamente";
+	}else{
+		echo mysqli_errno($con)." ".  mysqli_error($con);
+	}	
+?>
